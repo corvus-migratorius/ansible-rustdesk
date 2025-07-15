3f840ca66be73c9420ad518b9cd12be6b706cba4
Sergey Malyuk (fogucoco) <malyuk.ss@genlab.llc>
2025-07-15 14:23:26 +0000

    create rustdesk install script

---
+++ files/install-rustdesk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# move executables to binary directory
+mv hhbr /usr/bin/hbbr
+mv hbbs /usr/bin/hbbs
+
+# move services to systemd services directory
+mv rustdesk-hbbr.service /usr/lib/systemd/system/
+mv rustdesk-hbbs.service /usr/lib/systemd/system/
+
+# create log and working diretories
+mkdir -p /var/log/rustdesk-server
+mkdir -p /var/lib/rustdesk-server
+
+# reload systemd, start and enable services
+systemctl daemon-reload
+
+systemctl start rustdesk-hbbr
+systemctl start rustdesk-hbbs
+
+systemctl enable rustdesk-hbbr
+systemctl enable rustdesk-hbbs
